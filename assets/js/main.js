e93e37494932f9b1acf590ac5d162c763db02dfd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-22 14:35:16 +0100

    included further stations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,22 @@
-function getData(station, direction, container) {
+function getData(station, direction, container, ...filterValues) {
   const url = `https://1.bvg.transport.rest/stations/${station}/departures?direction=${direction}&duration=60`;
   fetch(url)
     .then(response => {
-      console.log(response);
       return response.json();
     }).then(data => {
-      console.log(data);
-      DisplayData(data, container);
+      if (filterValues) {
+        const filteredData = filterArray(data, filterValues);
+        DisplayData(filteredData, container);
+      } else {
+        DisplayData(data, container);
+      }
     }).catch(function (err) {
       console.log(`Something went wrong: ${err}`);
     });
 }
 
 function DisplayData(data, container) {
+  console.log(data);
   const containerTag = document.querySelector(container);
   if (data) {
     const stopName = data[0].stop.name;
@@ -20,6 +24,7 @@ function DisplayData(data, container) {
     const header = `<h2>Abfahrten ab ${stopName}</h2>`;
     const subheader = `<h3>Richtung ${directionName}</h3>`;
     const dataString = data.map(route => {
+      console.log(`${route.when}\n${route.line.name} ${route.direction}`);
       const realtime = route.when.substr(11, 5);
       const delay = Math.floor(route.delay / 60);
       const plantime = getPlanTime(realtime, delay);
@@ -55,9 +60,26 @@ function getPlanTime(realtime, delay) {
   return `${hourString}:${minuteString}`;
 }
 
-getData(900000160541, 900000160014, "#JOVStorkresults");
-getData(900000160541, 900000160529, "#JOVObfresults");
-getData(900000160509, 900000160710, "#BaeRoeresults");
-getData(900000160509, 900000160518, "#BaeHerresults");
-getData(900000160014, 900000160010, "#MoeRoeresults");
-getData(900000160014, 900000160017, "#MoeLoeresults");
+function filterArray(array, filterValues) {
+  const filtered = array.filter(route => {
+    let identifier = false;
+    filterValues.forEach(filterValue => {
+      if (route.line.name.includes(filterValue) || route.direction.includes(filterValue)) {
+        identifier = true;
+      }
+    });
+    if (identifier == true) {
+      return route;
+    }
+  });
+  return filtered;
+}
+
+getData(900000160541, 900000160014, "#JOVStorkresults", []);
+getData(900000160541, 900000160529, "#JOVObfresults", []);
+getData(900000160509, 900000160710, "#BaeRoeresults", []);
+getData(900000160509, 900000160518, "#BaeHerresults", []);
+getData(900000160014, 900000160010, "#MoeRoeresults", []);
+getData(900000160014, 900000160017, "#MoeLoeresults", []);
+getData(900000110012, 900000120001, "#BSTO_BFA", "S41", "Zeuthen", "Grünau", "Ostkreuz", "Schöneweide");
+getData(900000110012, 900000110004, "#BSTO_BLST", []);
