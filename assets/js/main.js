311e480905b462b3af2d2effeff0122adfd6ac79
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-22 11:08:38 +0100

    wrote results to the DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,30 @@ fetch(url)
   .then(response => {
     return response.json();
   }).then(data => {
-    console.log(data);
+    DisplayData(data, "#results");
   }).catch(function (err) {
     console.log(`Something went wrong: ${err}`);
   });
+
+function DisplayData(data, container) {
+  const containerTag = document.querySelector(container);
+  if (data) {
+    const stopName = data[0].stop.name;
+    const header = `<h1>Abfahrten ab ${stopName}</h1>`;
+    const dataString = data.map(route => {
+      const time = route.when.substr(11, 5);
+      const delay = Math.floor(route.delay / 60);
+      const line = route.line.name;
+      const target = route.direction;
+      return `
+      <div class="row">
+      <div class="realTime">${time}</div>
+      <div class="delay">${delay}</div>
+      <div class="line">${line}</div>
+      <div class="target">${target}</div>
+      </div>`
+    }).join("");
+    const html = header + dataString;
+    containerTag.innerHTML = html;
+  }
+}
