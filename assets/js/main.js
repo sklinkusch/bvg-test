627a48f34158c5bfe29495ed3ffb6e16ec089646
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-23 23:40:17 +0100

    shortened definition of class Station_filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,52 +81,11 @@ class Station_nofilter {
   }
 }
 
-class Station_filter {
+class Station_filter extends Station_nofilter {
   constructor(domSelectors, station, directions, filterValues) {
-    this.containers = domSelectors.map(domSelector => document.querySelector(domSelector));
-    this.stop = station;
-    this.directions = directions;
+    super(domSelectors, station, directions);
     this.filter = filterValues;
     this.getData();
-  }
-  evalData(array, dirIndex) {
-    if (array.length > 0) {
-      let header;
-      if (dirIndex == 0) {
-        header = `<h2>Abfahrten ab ${array[0].stop.name}</h2>`;
-      } else {
-        header = "";
-      }
-      const subheader = `<h3>Richtung ${array[0].direction}</h3>`;
-      const innerString = array.map(route => {
-        let planTime, realTime, delay;
-        if (route.when != null && route.delay != null) {
-          realTime = route.when.substr(11, 5);
-          delay = Math.floor(route.delay / 60);
-          planTime = this.getPlanTime(realTime, delay);
-        } else if (route.when != null) {
-          realTime = route.when.substr(11, 5);
-          delay = "?";
-          planTime = realTime;
-        } else {
-          realTime = "Ausfall";
-          delay = "X";
-          planTime = route.formerScheduledWhen.substr(11, 5);
-        }
-        const line = route.line.name;
-        const target = route.direction;
-        return `
-        <div class="row">
-        <div class="planTime">${planTime}</div>
-      <div class="realTime">${realTime}</div>
-      <div class="delay">${delay}</div>
-      <div class="line">${line}</div>
-      <div class="target">${target}</div>
-        </div>
-        `;
-      }).join("");
-      this.containers[dirIndex].innerHTML = `${header}${subheader}${innerString}`;
-    }
   }
   filterArray(data, dirIndex) {
     if (this.filter[dirIndex].length > 0) {
@@ -159,131 +118,6 @@ class Station_filter {
           console.log(err);
         });
     });
-  }
-  getPlanTime(realtime, delay) {
-    let hours = Number(realtime.substr(0, 2));
-    let minutes = Number(realtime.substr(3, 2));
-    minutes -= delay;
-    if (minutes < 0) {
-      minutes += 60;
-      hours -= 1;
-    }
-    if (minutes > 59) {
-      minutes -= 60;
-      hours += 1;
-    }
-    if (hours < 0) {
-      hours += 24;
-    }
-    if (hours > 23) {
-      hours -= 24;
-    }
-    let hourString = hours < 10 ? `0${hours}` : `${hours}`;
-    let minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    return `${hourString}:${minuteString}`;
-  }
-}
-
-class Station {
-  constructor(domSelector, station, direction, filterValues) {
-    this.container = document.querySelector(domSelector);
-    this.stop = station;
-    this.direction = direction;
-    this.filter = filterValues;
-    this.getData();
-  }
-
-  displayData(data) {
-    if (data.length > 0) {
-      const stopName = data[0].stop.name;
-      const directionName = data[0].direction;
-      const header = `<h2>Abfahrten ab ${stopName}</h2>`;
-      const subheader = `<h3>Richtung ${directionName}</h3>`;
-      const dataString = data.map(route => {
-        let planTime, realTime, delay;
-        if (route.when != null && route.delay != null) {
-          realTime = route.when.substr(11, 5);
-          delay = Math.floor(route.delay / 60);
-          planTime = this.getPlanTime(realTime, delay);
-        } else if (route.when != null) {
-          realTime = route.when.substr(11, 5);
-          delay = "?";
-          planTime = realTime;
-        } else {
-          realTime = "Ausfall";
-          delay = "X";
-          planTime = route.formerScheduledWhen.substr(11, 5);
-        }
-        const line = route.line.name;
-        const target = route.direction;
-        return `
-        <div class="row">
-        <div class="planTime">${planTime}</div>
-      <div class="realTime">${realTime}</div>
-      <div class="delay">${delay}</div>
-      <div class="line">${line}</div>
-      <div class="target">${target}</div>
-        </div>
-        `;
-      }).join("");
-      const html = header + subheader + dataString;
-      this.container.innerHTML = html;
-    }
-  }
-
-  filterArray(data) {
-    const filtered = data.filter(route => {
-      let identifier = false;
-      this.filter.forEach(filterValue => {
-        if (route.line.name.includes(filterValue) || route.direction.includes(filterValue)) {
-          identifier = true;
-        }
-      });
-      if (identifier == true) {
-        return route;
-      }
-    });
-    return filtered;
-  }
-
-  getData() {
-    const url = `https://1.bvg.transport.rest/stations/${this.stop}/departures?direction=${this.direction}&duration=60`;
-    fetch(url)
-      .then(response => {
-        return response.json();
-      }).then(data => {
-        if (this.filter.length > 0) {
-          const filteredData = this.filterArray(data);
-          this.displayData(filteredData);
-        } else {
-          this.displayData(data);
-        }
-      }).catch(function (err) {
-        console.log(`Something went wrong. ${err}`);
-      });
-  }
-
-  getPlanTime(realtime, delay) {
-    let hours = Number(realtime.substr(0, 2));
-    let minutes = Number(realtime.substr(3, 2));
-    minutes -= delay;
-    if (minutes < 0) {
-      minutes += 60;
-      hours -= 1;
-    }
-    if (minutes > 59) {
-      minutes -= 60;
-      hours += 1;
-    }
-    if (hours < 0) {
-      hours += 24;
-    }
-    if (hours > 23) {
-      hours -= 24;
-    }
-    let hourString = hours < 10 ? `0${hours}` : `${hours}`;
-    let minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    return `${hourString}:${minuteString}`;
   }
 }
 
