73ec48198511439243ba61d451d9ca90ba65660b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-24 18:20:31 +0100

    reduced number of fetch requests with new class station

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,10 +123,106 @@ class Station_filter extends Station_nofilter {
   }
 }
 
-const jov = new Station_nofilter(["#JOVStorkresults", "#JOVObfresults"], 900000160541, [900000160014, 900000160529]);
-const bae = new Station_nofilter(["#BaeRoeresults", "#BaeHerresults"], 900000160509, [900000160710, 900000160518]);
-const moe = new Station_nofilter(["#MoeRoeresults", "#MoeLoeresults"], 900000160014, [900000160010, 900000160017]);
-const bsto = new Station_filter(["#BSTO_BFA", "#BSTO_BLST", "#BSTO_240"], 900000110012, [900000120001, 900000110004, 900000160014], [["S41", "S8", "S85"], ["S42", "S8", "S85"], []]);
-const bmn = new Station_filter(["#BMN_BWES", "#BMN_BWKRR", "#BMN_Zoo", "#BMN_Spa"], 900000024106, [900000026207, 900000024102, 900000024104, 900000032402], [["S41", "S46"], ["S42", "S46"], [], []]);
-const gat = new Station_nofilter(["#Gat_BHST", "#Gat_Kla", "#Gat_Sta", "#Gat_Spa", "#Gat_Pic"], 900000032106, [900000026105, 900000032251, 900000032102, 900000032104, 900000032451]);
-const wil = new Station_nofilter(["#Wil_Spa", "#Wil_Kla", "#Wil_Pdm", "#Wil_See"], 900000032102, [900000032104, 900000039101, 900000220301, 900000220347]);
+class Station {
+  constructor(domSelector, station, filterValues) {
+    this.container = document.querySelector(domSelector);
+    this.stop = station;
+    this.filter = filterValues;
+    this.getData();
+  }
+  evalData(array) {
+    if (array.length > 0) {
+      const header = document.createElement('h2');
+      header.innerHTML = `Abfahrten ab ${array[0].stop.name}`;
+      this.container.appendChild(header);
+
+      this.filter.forEach(filterSet => {
+        var subheader_ident = true;
+        for (let i = 0; i < array.length; i++) {
+          let identifier = false;
+          for (let j = 0; j < filterSet.length; j++) {
+            if (array[i].line.name.includes(filterSet[j]) || array[i].direction.includes(filterSet[j])) {
+              identifier = true;
+              break;
+            }
+          }
+          if (identifier == true) {
+            if (subheader_ident == true) {
+              const subheader = document.createElement('h3');
+              subheader.innerHTML = `Richtung ${array[i].direction}`;
+              this.container.appendChild(subheader);
+              subheader_ident = false;
+            }
+            let planTime, realTime, delay;
+            if (array[i].when != null && array[i].delay != null) {
+              realTime = array[i].when.substr(11, 5);
+              delay = Math.floor(array[i].delay / 60);
+              planTime = this.getPlanTime(realTime, delay);
+            } else if (array[i].when != null) {
+              realTime = array[i].when.substr(11, 5);
+              delay = "?";
+              planTime = realTime;
+            } else {
+              realTime = "Ausfall";
+              delay = "X";
+              planTime = array[i].formerScheduledWhen.substr(11, 5);
+            }
+            const line = array[i].line.name;
+            const target = array[i].direction;
+            const row = document.createElement('div');
+            row.className = "row";
+            row.innerHTML = `
+            <div class="planTime">${planTime}</div>
+            <div class="realTime">${realTime}</div>
+            <div class="delay">${delay}</div>
+            <div class="line">${line}</div>
+            <div class="direction">${target}</div>
+            `;
+            this.container.appendChild(row);
+          }
+        }
+      });
+    }
+  }
+  getData() {
+    const url = `https://1.bvg.transport.rest/stations/${this.stop}/departures?duration=60&includeRelatedStations=false`;
+    fetch(url)
+      .then(response => {
+        return response.json();
+      }).then(data => {
+        this.evalData(data);
+      }).catch(function (err) {
+        console.log(err);
+      });
+  }
+  getPlanTime(realtime, delay) {
+    let hours = Number(realtime.substr(0, 2));
+    let minutes = Number(realtime.substr(3, 2));
+    minutes -= delay;
+    if (minutes < 0) {
+      minutes += 60;
+      hours -= 1;
+    }
+    if (minutes > 59) {
+      minutes -= 60;
+      hours += 1;
+    }
+    if (hours < 0) {
+      hours += 24;
+    }
+    if (hours > 23) {
+      hours -= 24;
+    }
+    let hourString = hours < 10 ? `0${hours}` : `${hours}`;
+    let minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    return `${hourString}:${minuteString}`;
+  }
+}
+
+const jov = new Station("#JOV", 900000160541, [["Storkower", "Hugenottenplatz"], ["Ostbahnhof", "Betriebshof", "Tierpark"]]);
+const bae = new Station("#Bae", 900000160509, [["Hauptbahnhof", "Petersburger", "Schöneweide"], ["Ahrensfelde", "Marzahn", "Gudrunstr"]]);
+const moe = new Station("#Moe", 900000160014, [["Warschauer", "Frankfurter", "Schöneweide"], ["Virchow", "Gudrunstr", "Riesaer"]]);
+const bsto = new Station("#BSTO", 900000110012, [["S41", "Grünau", "Ostkreuz", "Schöneweide", "Zeuthen"], ["S42", "Birkenwerder", "Blankenburg", "Pankow", "Greifswalder"], ["Ostbahnhof", "Betriebshof"]]);
+const bmn = new Station("#BMN", 900000024106, [["S41", "Westend", "Gesundbrunnen"], ["S42", "Halensee", "Wusterhausen"], ["Gatower", "Gutsstr", "Hottengrund", "Hahneberg", "Spektefeld", "Nennhauser", "Reimerweg"], ["Zoo", "Wilmersdorfer"]]);
+const gat = new Station("#Gat", 900000032106, [["Zoo", "Wilmersdorfer"], ["Hottengrund", "Alt-Gatow", "Alt-Kladow", "Gutsstr"], ["Nennhauser", "Reimerweg", "Hahneberg", "Spektefeld"], ["Wasserwerk", "Kisseln"], ["Aalemannufer", "Hennigsdorf", "Werderstr", "Haselhorst"]]);
+const wil = new Station("#Wil", 900000032102, [["Rathaus"], ["Alt-Kladow"], ["Potsdam", "Jungfernsee"], ["Seeburg", "Havelpark"]]);
